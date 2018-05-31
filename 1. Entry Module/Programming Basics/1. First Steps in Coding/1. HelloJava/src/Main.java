6f9475a5baffb94e76a4d8769a0f8dfb1ddfc27b
uagg <lucifercheto@abv.bg>
2018-05-31 15:17:25 +0300

    Solving upload problems

--- 1. Entry Module/Programming Basics/1. First Steps in Coding/1. HelloJava/src/Main.java
+++
@@ -1,8 +0,0 @@
-/*
-Напишете конзолна Java програма, която отпечатва текста “Hello Java”.
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
